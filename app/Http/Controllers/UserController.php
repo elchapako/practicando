cc50921d7d41f5a812fbc93f0802357d2827d575
Edwin Roger Ibañez Mendieta <el.chapako@gmail.com>
2018-11-25 23:12:42 -0500

    views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        return 'Usuarios';
+        $users = [
+          'Edwin',
+          'Nestor',
+          'Marcia',
+          'Adolfo',
+          'Moises',
+            '<script>alert("Clicker")</script>'
+        ];
+
+        $title = "Listado de usuarios";
+
+        return view('users', compact('title', 'users'));
     }
 
     public function show($id)
