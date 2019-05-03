45c8fcf39daa12b7ec1a7b6aba87da3b1b0c2c85
Edwin Roger Ibañez Mendieta <el.chapako@gmail.com>
2019-05-02 23:42:09 -0500

    add boostrap styles in users list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,29 +3,46 @@
 @section('title', 'Usuarios')
 
 @section('content')
+    <div class="d-flex justify-content-between align-items-end mb-3">
+        <h1 class="pb-1">{{ $title }}</h1>
 
-    <h1>{{ $title }}</h1>
+        <p>
+            <a href="{{route('users.create')}}" class="btn btn-primary">Nuevo Usuario</a>
+        </p>
+    </div>
 
-    <p>
-        <a href="{{route('users.create')}}">Nuevo Usuario</a>
-    </p>
-
-            <ul>
-                @forelse ($users as $user)
-                    <li>
-                        {{ $user->name }}, ({{ $user->email }})
-                        <a href="{{ route('users.show', $user) }}">Ver detalles</a> |
-                        <a href="{{ route('users.edit', $user) }}">Editar</a> |
-                        <form action="{{ route('users.destroy', $user) }}" method="post">
-                            {{ csrf_field() }}
-                            {{ method_field('delete') }}
-                            <button type="submit">Eliminar</button>
-                        </form>
-                    </li>
-                @empty
-                    <p>No hay usuarios registrados.</p>
-                @endforelse
-            </ul>
+    @if($users->isNotEmpty())
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+        <tr>
+            <th scope="row">{{ $user->id }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>
+                <form action="{{ route('users.destroy', $user) }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('delete') }}
+                    <a href="{{ route('users.show', $user) }}" class="btn btn-link"><span class="oi oi-eye"></span></a>
+                    <a href="{{ route('users.edit', $user) }}"><span class="oi oi-pencil"></span></a>
+                    <button type="submit" class="btn btn-link"><span class="oi oi-trash"></span></button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+    @else
+        <p>No hay usuarios registrados.</p>
+    @endif
 @endsection
 
 @section('sidebar')
