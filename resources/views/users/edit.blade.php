54402cdfa38fb07ba707106ad38413960734eddc
Edwin Roger Ibañez Mendieta <el.chapako@gmail.com>
2019-05-05 18:14:24 -0500

    changing blande for edit and view

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,36 +3,43 @@
 @section('title', "Crear usuario")
 
 @section('content')
-    <h1>Editar Usuario</h1>
 
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <h6>Por favor corrige los errores de abajo</h6>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
+    <div class="card">
+        <h4 class="card-header">Editar Usuario</h4>
+        <div class="card-body">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <h6>Por favor corrige los errores de abajo</h6>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form method="post" action="{{ url("usuarios/{$user->id}") }}">
+                {{ method_field('put') }}
+                {!! csrf_field() !!}
+
+                <div class="form-group">
+                    <label for="name">Nombre:</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Pedro Perez" value="{{old('name', $user->name)}}">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="pedro@example.com" value="{{old('email', $user->email)}}">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Mayor a 6 caracteres">
+                </div>
+                <button type="submit" class="btn btn-primary">Actualizar usuario</button>
+                <a href="{{ route('users') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+            </form>
         </div>
-    @endif
-
-    <form method="post" action="{{ url("usuarios/{$user->id}") }}">
-        {{ method_field('put') }}
-        {{ csrf_field() }}
-
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" placeholder="Pedro Perez" value="{{old('name', $user->name)}}">
-        <br>
-        <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{old('email', $user->email)}}">
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
-        <br>
-        <button type="submit">Actualizar usuario</button>
-    </form>
-    <p>
-        <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
-    </p>
+    </div>
 @endsection
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,10 +3,27 @@
 @section('title', "Usuario {$user->id}")
 
 @section('content')
-    <h1>Usuario #{{ $user->id }}</h1>
 
-    <p>Nombre del usuario: {{ $user->name }} </p>
-    <p>Correo electrónico: {{ $user->email }} </p>
+    <div class="d-flex justify-content-between align-items-end mb-3">
+        <h1 class="pb-1">Usuario #{{ $user->id }}</h1>
+    </div>
+
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+        </tbody>
+    </table>
     <p>
         <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
     </p>
